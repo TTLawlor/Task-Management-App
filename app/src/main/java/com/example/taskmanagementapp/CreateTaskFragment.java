dab6cc4856242416db122e23a587eff80fd00bfb
TTLawlor <lawlortamara@gmail.com>
2024-04-21 14:40:29 +0100

    updated create task xml file. Adding date and time pickers

--- app/src/main/java/com/example/taskmanagementapp/CreateTaskFragment.java
+++ app/src/main/java/com/example/taskmanagementapp/CreateTaskFragment.java
@@ -1,6 +1,23 @@
 package com.example.taskmanagementapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class CreateTaskFragment extends Fragment {
+    // Requires an empty public constructor
+    public CreateTaskFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_create_task,container,false);
+        return view;
+    }
 }
